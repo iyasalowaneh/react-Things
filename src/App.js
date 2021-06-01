86183c4404d63d6645d46b989f1a45524518a898
iyasalowaneh <iyas_moh@yahoo.com>
2021-06-01 15:09:57 +0300

    added products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from "./logo.svg";
+import products from "./products";
+
 import "./App.css";
 
 function App() {
+  const productArray = products.map((product) => (
+    <div>
+      <img className="image" src={product.image} alt={product.name} />
+      <h1>{product.name}</h1>
+      <p>{product.price} $</p>
+    </div>
+  ));
   return (
     <div>
-      <h className="App-header">Things</h>
-      <p className="App">i love my website</p>
-      <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-688899881-1519413300.jpg" />
+      <h className="app-header">Things</h>
+      <p className="app">i love my website</p>
+      <img
+        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-688899881-1519413300.jpg"
+        alt="NYC"
+      />
+
+      <div className="product-list">{productArray}</div>
     </div>
   );
 }
